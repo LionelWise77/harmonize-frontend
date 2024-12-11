fc5b63eae14be6c0a44877d0bc873e26d2f5fa94
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-12-11 16:03:23 +0000

    added function to tasks.js settasks, usestate

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../utils/utils";
 
 function Tasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get("tasks/")
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  }, []);
+
   return (
-    <div className="container mt-5">
-      <h1>Your Tasks</h1>
-      <p>Here you can view and manage your tasks.</p>
+    <div className="container mt-4">
+      <h1>Tasks</h1>
+      {tasks.length > 0 ? (
+        <ul className="list-group">
+          {tasks.map((task) => (
+            <li key={task.id} className="list-group-item">
+              <h5>{task.title}</h5>
+              <p>{task.description}</p>
+              <small>Due Date: {task.due_date}</small>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tasks available.</p>
+      )}
     </div>
   );
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,10 +2,18 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    "http://8000-lionelwise7-harmonizeba-p4p9ravs0w8.ws.codeinstitute-ide.net/api/", // Reemplaza con la URL base de tu backend
+    "http://8000-lionelwise7-harmonizeba-p4p9ravs0w8.ws.codeinstitute-ide.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
