1b219b25e35d6c049608d0fa53ee9afbfd9cabfc
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-12-13 19:58:10 +0000

    changue url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://harmonize-backend-6405d8cae271.herokuapp.com/api/";
+axios.defaults.baseURL = "https://harmonize-backend-6405d8cae271.herokuapp.com";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true; //
