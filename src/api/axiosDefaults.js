347885633c2219cf4504f7fbc3e01d9d56e498a7
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-12-27 15:30:01 +0000

    succesfull login

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,18 +3,18 @@ import axios from "axios";
 const isLocal = window.location.hostname === "localhost";
 const baseURL = isLocal
   ? "http://localhost:8000/"
-  : "https://8000-lionelwise7-harmonizeba-rqih9n6nle6.ws.codeinstitute-ide.net/";
+  : "https://8000-lionelwise7-harmonizeba-futj17df98x.ws.codeinstitute-ide.net/";
 
-axios.defaults.baseURL = `${baseURL}api/`;
+axios.defaults.baseURL = `${baseURL}`;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true; // Incluye cookies en las solicitudes
 
 export const axiosReq = axios.create({
-  baseURL: `${baseURL}api/`,
+  baseURL: `${baseURL}`,
 });
 
 export const axiosRes = axios.create({
-  baseURL: `${baseURL}api/`,
+  baseURL: `${baseURL}`,
 });
 
 export default axios;
