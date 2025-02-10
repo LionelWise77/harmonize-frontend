15ce3b5cf59da48bbe0e97a8bf38edd8367831b9
LioneWise77 <sebbepebe27@gmail.com>
2025-02-10 22:27:48 +0100

    migration complete to VS code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://8000-lionelwise7-harmonizeba-39ldxkh93bi.ws.codeinstitute-ide.net";
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
